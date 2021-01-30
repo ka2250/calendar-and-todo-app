a00ad5da994541770fb0ed1e34e67c807a4b488d
ka2250 <katok2250@gmail.com>
2021-01-30 19:38:56 +0900

    fix: Gridコンポーネントで囲ってuseStylesでスタイルを適用

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 
 const Header = () => {
 	return (
 		<>
-			<Typography variant="h5" display="block">
-				CalendarToDo
-			</Typography>
-			<Typography variant="caption">
-				Add a Schedule and a Todos
-			</Typography>
+			<Grid>
+				<Grid item>
+					<Typography variant="h5">CalendarToDo</Typography>
+
+					<Typography variant="caption">
+						Add a Schedule and a Todos
+					</Typography>
+				</Grid>
+			</Grid>
 		</>
 	);
 };
