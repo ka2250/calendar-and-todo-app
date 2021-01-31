0aeace3a03041a5cc0bb4f54f966a12b645a6fac
ka2250 <katok2250@gmail.com>
2021-01-31 23:21:23 +0900

    fix: スターアイコンを追加

--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 
-import { Card, CardContent, Typography } from "@material-ui/core";
+import {
+	Box,
+	Card,
+	CardContent,
+	IconButton,
+	Typography,
+} from "@material-ui/core";
+import GradeIcon from "@material-ui/icons/Grade";
+
+import { theme } from "../utils/theme";
 
 const TextCard = (props) => {
 	return (
 		<Card>
 			<CardContent>
-				<Typography variant="body1">{props.title}</Typography>
+				<Box
+					display="flex"
+					justifyContent="flex-start"
+					alignItems="center"
+					style={theme.box}
+				>
+					<IconButton
+						area-label="favorite"
+						size="small"
+						style={theme.iconButton}
+					>
+						<GradeIcon />
+					</IconButton>
+					<Typography variant="subtitle1">{props.title}</Typography>
+				</Box>
+				<Typography variant="subtitle2">{props.body}</Typography>
 			</CardContent>
 		</Card>
 	);
