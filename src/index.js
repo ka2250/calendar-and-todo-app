cc1fd0a16efbec028964e0276c23df9f31d7606d
ka2250 <katok2250@gmail.com>
2021-02-03 15:55:59 +0900

    delete React.StrictMode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import DateFnsUtils from "@date-io/date-fns";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 
 ReactDOM.render(
-	<React.StrictMode>
+	<>
 		<MuiPickersUtilsProvider utils={DateFnsUtils}>
 			<App />
 		</MuiPickersUtilsProvider>
-	</React.StrictMode>,
+	</>,
 	document.getElementById("root")
 );
 
