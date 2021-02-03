eb45dce689b4d051bcc104dd4baae3b37d0564d6
ka2250 <katok2250@gmail.com>
2021-02-03 15:56:48 +0900

    action.dateをCREATE_EVENTに追加

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -2,14 +2,25 @@ const eventReducer = (state = [], action) => {
 	// state　は配列，　action　events(ex. クリックなど)
 	switch (action.type) {
 		case "CREATE_EVENT":
-			const event = { title: action.title, body: action.body };
+			const event = {
+				title: action.title,
+				body: action.body,
+				date: action.date,
+			};
 			const length = state.length;
 			const id = length === 0 ? 1 : state[length - 1].id + 1;
 
-			return [...state, { id, ...event }];
+			return [{ id, ...event }, ...state];
 
 		case "DELETE_EVENT":
 			return state.filter((event) => event.id !== action.id);
+
+		case "COMPLETE_EVENT":
+			const completeEvents = {
+				description: action.description,
+			};
+
+			return [completeEvents, ...state];
 
 		default:
 			return state;
