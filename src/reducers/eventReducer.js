8d300522d778643d4d48d0313e126b00d2b74583
ka2250 <katok2250@gmail.com>
2021-02-05 02:43:07 +0900

    fix: length, idの定義のスコープを少し変更

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -1,5 +1,9 @@
 const eventReducer = (state = [], action) => {
 	// state　は配列，　action　events(ex. クリックなど)
+
+	const length = state.length;
+	const id = length + 1;
+
 	switch (action.type) {
 		case "CREATE_EVENT":
 			const event = {
@@ -7,8 +11,6 @@ const eventReducer = (state = [], action) => {
 				body: action.body,
 				date: action.date,
 			};
-			const length = state.length;
-			const id = length === 0 ? 1 : state.id + 1;
 
 			return [{ id, ...event }, ...state];
 
