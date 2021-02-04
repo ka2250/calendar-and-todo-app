5eccf0a8611b3cc776820704d1b9917dc47869b8
ka2250 <katok2250@gmail.com>
2021-02-05 01:25:31 +0900

    delete "COMPLETE_EVENT"

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -8,19 +8,15 @@ const eventReducer = (state = [], action) => {
 				date: action.date,
 			};
 			const length = state.length;
-			const id = length === 0 ? 1 : state[length - 1].id + 1;
+			const id = length === 0 ? 1 : state.id + 1;
 
 			return [{ id, ...event }, ...state];
 
 		case "DELETE_EVENT":
 			return state.filter((event) => event.id !== action.id);
 
-		case "COMPLETE_EVENT":
-			const completeEvents = {
-				description: action.description,
-			};
-
-			return [completeEvents, ...state];
+		case "DELETE_ALL":
+			return [];
 
 		default:
 			return state;
